a2bb5e7b7d6ca667c32c7629e3477c6dfd9e3fa1
Peter Phanouvong <peterphanouvong@gmail.com>
2024-11-21 14:16:39 +1100

    reset

--- kindeSrc/environment/workflows/Workflow.ts
+++ kindeSrc/environment/workflows/Workflow.ts
@@ -60,7 +60,7 @@ const handler = {
       book: Book;
     }>();
 
-    accessToken.book = await fetchBook("OL24224314M");
+    accessToken.book = await fetchBook("your favorite book id");
   },
 };
 
